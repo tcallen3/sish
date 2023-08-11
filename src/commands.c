df164e46661aa3503dcb89db61287c02f09803a9
Thomas Allen <allen.thomas.c@gmail.com>
2023-08-11 01:09:47 -0700

    flesh out built-in commands for shell

--- src/commands.c
+++ src/commands.c
@@ -31,6 +31,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
+#include <errno.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "commands.h"
 
 #define REPEAT 1
@@ -40,16 +46,86 @@ static const char CD_CMD[] = "cd";
 static const char ECHO_CMD[] = "echo";
 static const char EXIT_CMD[] = "exit";
 
+static char *
+get_passwd_home()
+{
+	struct passwd *pass_ent;
+	uid_t uid;
+
+	uid = getuid();
+	if ((pass_ent = getpwuid(uid)) == NULL) {
+		perror("cd");
+		return NULL;
+	}
+
+	if (pass_ent->pw_dir == NULL) {
+		fprintf(stderr, "cd: no home dir defined for user\n");
+		return NULL;
+	}
+	
+	return pass_ent->pw_dir;
+}
+
 static int
 change_dir(char **tokens, size_t tokens_len)
 {
-	/* FIXME: implement */
+	char *dir = NULL;
+	const char var_name[] = "HOME";
+	if (tokens_len > 2) {
+		fprintf(stderr, "cd: too many arguments\n");
+		return 1;
+	} else if (tokens_len == 2) {
+		dir = tokens[1];
+	} else {
+		/* only cd specified, so try changing to $HOME */
+		dir = getenv(var_name);
+
+		/* $HOME isn't set, so change to user dir in getpwuid(3) */
+		if (dir == NULL) {
+			if ((dir = get_passwd_home()) == NULL) {
+				return 1;
+			}
+
+		}
+	}
+
+	if (chdir(dir) != 0) {
+		perror("cd");
+		return 1;
+	}
+
+	return 0;
 }
 
 static int
 echo_line(char **tokens, size_t tokens_len, int *status)
 {
-	/* FIXME: implement */
+	char *word;
+	const char stat_var[] = "$?";
+	const char pid_var[] = "$$";
+	pid_t pid;
+	const size_t var_len = 2;
+	size_t idx = 1;
+
+	while (idx < tokens_len) {
+		word = tokens[idx];
+		idx++;
+
+		if (strncmp(word, stat_var, var_len) == 0) {
+			printf("%d ", *status);
+			continue;
+		} else if (strncmp(word, pid_var, var_len) == 0) {
+			pid = getpid();
+			printf("%d ", pid);
+			continue;
+		}
+
+		printf("%s ", word);
+	}
+
+	printf("\n");
+
+	return 0;
 }
 
 int 
